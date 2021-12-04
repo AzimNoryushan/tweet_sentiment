b92d24cf0bdf5cdeef4b0868af570b8570e34ae6
azimnoryushanplus <azim.noryushan@plus.com.my>
2021-12-05 00:08:59 +0800

    duhhh

--- topic_sentiment.py
+++ topic_sentiment.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt
-import traceback
+#import matplotlib.pyplot as plt
+#import traceback
 from sentiment_detection import Sentiment_detection
 from twint_service import Twint_service
-from datetime import date
+#from datetime import date
 import time
 
 class Topic_sentiment():
@@ -26,8 +26,6 @@ class Topic_sentiment():
 
         tweets = Twint_service().getTweets(topic)
 
-        time.sleep(20)
-
         for tweet in tweets:
             try:
                 sentiment = self.listToString(Sentiment_detection().getSentiment([tweet['tweet']]))
@@ -39,25 +37,25 @@ class Topic_sentiment():
                     neutral_results = neutral_results + 1
                 else:
                     undetected_results = undetected_results+1
-            except:
-                print(traceback.print_exc())
+            except Exception as e:
+                print(e)
 
-        self.generate_chart(positive_results, negative_results, neutral_results, topic)
+        #self.generate_chart(positive_results, negative_results, neutral_results, topic)
 
         return { "positive": positive_results, "negative": negative_results, "neutral": neutral_results}
         #return "Positive: " + str(positive_results) + " Negative: " + str(negative_results) + " Neutral: " + str(neutral_results)
 
-    def generate_chart(self, positive_results, negative_results, neutral_results, topic):
-        try:
-            today = date.today()
-            fig = plt.figure()
-            ax = fig.add_axes([0,0,1,1])
-            ax.axis('equal')
-            sentiment_list = ['positive', 'negative', 'neutral']
-            data_count = [positive_results, negative_results, neutral_results]
-            colors = ['green', "red", "blue"]
-            ax.pie(data_count, labels = sentiment_list,autopct='%1.2f%%',colors=colors)
-            plt.title("Sentiment of " + topic + " in Twitter on " +  today.strftime("%d/%m/%Y"))
-            plt.savefig('img/plot3.png', dpi=300, bbox_inches='tight')
-        except:
-            print(traceback.print_exc())
+    # def generate_chart(self, positive_results, negative_results, neutral_results, topic):
+    #     try:
+    #         today = date.today()
+    #         fig = plt.figure()
+    #         ax = fig.add_axes([0,0,1,1])
+    #         ax.axis('equal')
+    #         sentiment_list = ['positive', 'negative', 'neutral']
+    #         data_count = [positive_results, negative_results, neutral_results]
+    #         colors = ['green', "red", "blue"]
+    #         ax.pie(data_count, labels = sentiment_list,autopct='%1.2f%%',colors=colors)
+    #         plt.title("Sentiment of " + topic + " in Twitter on " +  today.strftime("%d/%m/%Y"))
+    #         plt.savefig('img/plot3.png', dpi=300, bbox_inches='tight')
+    #     except:
+    #         print(traceback.print_exc())
